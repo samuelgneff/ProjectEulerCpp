281175b3a7c9e00101f759fccecebef977175f73
Samuel Neff <neff.samuelg@gmail.com>
2023-04-07 12:52:03 -0600

    finished archived problem 2

--- out/build/CMakeFiles/Makefile.cmake
+++ out/build/CMakeFiles/Makefile.cmake
@@ -48,4 +48,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/Euler.dir/DependInfo.cmake"
+  "CMakeFiles/problem_2.dir/DependInfo.cmake"
   )
---
+++ out/build/CMakeFiles/problem_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_2.dir/problem_2.cc.o"
+  "CMakeFiles/problem_2.dir/problem_2.cc.o.d"
+  "problem_2"
+  "problem_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
